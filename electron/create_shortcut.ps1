bae3905a79c437ecbbe28e2949288b0fc091eae5
ZGup-Azmat <azmat1119688339@163.com>
2026-06-13 12:46:50 +0800

    fix: 桌面快捷方式改为 VBS 无窗口启动，关闭终端不再影响应用
    - 新增 launcher.vbs (run mode 0 = 隐藏窗口)
    - 快捷方式指向 VBS 而非 bat

--- electron/create_shortcut.ps1
+++ electron/create_shortcut.ps1
@@ -1,8 +1,8 @@
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut([Environment]::GetFolderPath('Desktop') + '\Tomato Timer.lnk')
-$Shortcut.TargetPath = 'C:\Users\BHkx\Desktop\first-cc\electron\start.bat'
+$Shortcut.TargetPath = 'C:\Users\BHkx\Desktop\first-cc\electron\launcher.vbs'
 $Shortcut.WorkingDirectory = 'C:\Users\BHkx\Desktop\first-cc\electron'
 $Shortcut.IconLocation = '%SystemRoot%\System32\shell32.dll,27'
 $Shortcut.Description = 'Tomato Timer - Pomodoro'
 $Shortcut.Save()
-Write-Output 'Desktop shortcut created!'
+Write-Output 'Desktop shortcut updated!'
