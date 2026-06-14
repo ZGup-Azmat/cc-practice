5fc2379f1fdd9517219510cade41434a89a89b57
ZGup-Azmat <azmat1119688339@163.com>
2026-06-14 20:25:35 +0800

    auto: session checkpoint 2026-06-14 20:25

--- electron/create_shortcut.ps1
+++ electron/create_shortcut.ps1
@@ -2,7 +2,7 @@ $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut([Environment]::GetFolderPath('Desktop') + '\Tomato Timer.lnk')
 $Shortcut.TargetPath = 'C:\Users\BHkx\Desktop\first-cc\electron\launcher.vbs'
 $Shortcut.WorkingDirectory = 'C:\Users\BHkx\Desktop\first-cc\electron'
-$Shortcut.IconLocation = '%SystemRoot%\System32\shell32.dll,27'
+$Shortcut.IconLocation = 'C:\Users\BHkx\Desktop\first-cc\static\tomato.ico'
 $Shortcut.Description = 'Tomato Timer - Pomodoro'
 $Shortcut.Save()
 Write-Output 'Desktop shortcut updated!'
